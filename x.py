239d0cddc7c2b5359618bcde206905d4f006f09b
Tat Nguyen Van <nguyenvantat1182002@gmail.com>
2024-11-18 20:15:13 +0700

    Update project files.

--- x.py
+++ x.py
@@ -69,9 +69,8 @@ class X:
         variables = {
             "userId": user_id,
             "count": 20,
-            "includePromotedContent": False,
-            "withClientEventToken": False,
-            "withBirdwatchNotes": False,
+            "includePromotedContent": True,
+            "withQuickPromoteEligibilityTweetFields": True,
             "withVoice": True,
             "withV2Timeline": True
         }
@@ -86,7 +85,7 @@ class X:
             'features': '{"rweb_tipjar_consumption_enabled":true,"responsive_web_graphql_exclude_directive_enabled":true,"verified_phone_label_enabled":false,"creator_subscriptions_tweet_preview_api_enabled":true,"responsive_web_graphql_timeline_navigation_enabled":true,"responsive_web_graphql_skip_user_profile_image_extensions_enabled":false,"communities_web_enable_tweet_community_results_fetch":true,"c9s_tweet_anatomy_moderator_badge_enabled":true,"articles_preview_enabled":true,"responsive_web_edit_tweet_api_enabled":true,"graphql_is_translatable_rweb_tweet_is_translatable_enabled":true,"view_counts_everywhere_api_enabled":true,"longform_notetweets_consumption_enabled":true,"responsive_web_twitter_article_tweet_consumption_enabled":true,"tweet_awards_web_tipping_enabled":false,"creator_subscriptions_quote_tweet_preview_enabled":false,"freedom_of_speech_not_reach_fetch_enabled":true,"standardized_nudges_misinfo":true,"tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled":true,"rweb_video_timestamps_enabled":true,"longform_notetweets_rich_text_read_enabled":true,"longform_notetweets_inline_media_enabled":true,"responsive_web_enhance_cards_enabled":false}',
             'fieldToggles': '{"withArticlePlainText":false}'
         }
-        response = self._request.get(f'{self._base_url}/HaouMjBviBKKTYZGV_9qtg/UserMedia', params=params, timeout=self._timeout)
+        response = self._request.get(f'{self._base_url}/Tg82Ez_kxVaJf7OPbUdbCg/UserTweets', params=params, timeout=self._timeout)
         
         videos = []
         cursor = None
@@ -94,12 +93,11 @@ class X:
         try:
             data = response.json()
             entries =  data['data']['user']['result']['timeline_v2']['timeline']['instructions'][-1]['entries']
-            
-            if len(entries) > 2:
-                items = entries[0]['content']['items']
 
-                for item in items:
-                    entities = item['item']['itemContent']['tweet_results']['result']['legacy']['entities']
+            for item in entries[:-2]:
+                content = item['content']
+                if 'itemContent' in content:
+                    entities = content['itemContent']['tweet_results']['result']['legacy']['entities']
                     if 'media'in entities:
                         entities = entities['media'][-1]
                         if 'video_info' in entities:
